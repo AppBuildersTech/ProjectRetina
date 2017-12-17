5f8a6dbadac6b47ef44fe7497a66b616f0960aa6
Alper Bayram <alperbayram2@gmail.com>
2017-12-17 23:29:31 +0300

    Clean Up

--- src/config.py
+++ src/config.py
@@ -13,11 +13,3 @@ detectionLabelPath = config.get("paths", "detectionLabelPath")
 samplePath = config.get("paths", "samplePath")
 classificationFeatures = config.get("paths", "classificationFeatures")
 detectionFeatures = config.get("paths", "detectionFeatures")
-
-# #  ------           [Compression]          ------------- #
-frame_length = config.getint ("compression", "frame_length")
-fs=config.getint ("compression", "fs")
-
-#  ------           [FEATURES]          ------------- #
-n_point = 1024
-fft_length = 245760
